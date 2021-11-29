8458a3503da31bffea06a80cdf546792a25f0541
mikevhe18 <viriyananda.michael@gmail.com>
2021-11-29 09:15:11 +0700

    Task: T/21/003968

--- bssb/models/account_asset_asset.py
+++ bssb/models/account_asset_asset.py
@@ -3,7 +3,7 @@
 # Copyright 2020 PT. Simetri Sinergi Indonesia
 # License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl).
 
-from openerp import models, fields
+from openerp import fields, models
 
 
 class AccountAssetAsset(models.Model):
@@ -12,5 +12,7 @@ class AccountAssetAsset(models.Model):
     operating_unit_id = fields.Many2one(
         string="Operating Unit",
         comodel_name="operating.unit",
-        default=lambda self: self.env['res.users'].operating_unit_default_get(self._uid),
+        default=lambda self: self.env["res.users"].operating_unit_default_get(
+            self._uid
+        ),
     )
