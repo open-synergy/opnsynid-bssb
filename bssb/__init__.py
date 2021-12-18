3e30817b2ec759927d61e66d227bbe6fed0c59d6
Andhitia Rama <andhitia.r@gmail.com>
2021-12-18 23:08:51 +0700

    [bssb] 8.0.1.5.0

--- bssb/__init__.py
+++ bssb/__init__.py
@@ -5,4 +5,5 @@
 
 from . import (
     models,
+    reports,
 )
---
+++ bssb/reports/__init__.py
@@ -0,0 +1,16 @@
+# -*- coding: utf-8 -*-
+# Copyright 2020 OpenSynergy Indonesia
+# Copyright 2020 PT. Simetri Sinergi Indonesia
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+
+from . import (
+    penerimaan_pembelian,
+    penerimaan_donasi,
+    penerimaan_sewa,
+    penerimaan_antar_kantor,
+    pemindahan_antar_lokasi,
+    pengeluaran_donasi,
+    pengeluaran_penjualan,
+    berita_acara_kehilangan,
+    berita_acara_pencurian,
+)
